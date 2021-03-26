64467941842669cbf30e8d71bb7a16cbb0714d83
Matthew Talluto <matthew.talluto@uibk.ac.at>
2021-03-26 12:20:59 +0100

    Network import feature/vignette

--- R/plot_print.R
+++ R/plot_print.R
@@ -56,8 +56,9 @@ plot.river_network = function(x, variable = 1, t, zlim, ...) {
 	args$x = igraph::graph_from_adjacency_matrix(adjacency(x), mode = "directed")
 
 	## weight the plot by discharge for the UPSTREAM node
-	wt = discharge(x)[Matrix::which(adjacency(x) != 0, arr.ind = TRUE)[,1]]
-#	wt = discharge(x)[2:nrow(adjacency(x))]
+	up_nodes = as.integer(igraph::ends(args$x, igraph::E(args$x))[,1])
+	wt = discharge(x)[up_nodes]
+
 	args$edge.width = (wt / max(wt)) * args$edge.width
 	if(!("layout" %in% args) && "layout" %in% names(attributes(x)))
 		args$layout = attr(x, "layout")
